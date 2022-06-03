14fe74f86bd60de5d7627964835ca19db69c4bc1
Ni Jingwei <jingweini8@gmail.com>
2022-06-03 22:36:24 +0100

    readme

--- code/exp_with_args.sh
+++ code/exp_with_args.sh
@@ -1,5 +1,5 @@
 WORKDIR="/cluster/work/sachan/leonhard/jingwei/ni2/MTL4CodeXGLUE/code"
-DATADIR="/cluster/work/sachan/leonhard/jingwei/ni2/MTL4CodeXGLUE"
+DATADIR="/cluster/work/sachan/leonhard/jingwei/ni2/MTL4CodeXGLUE/data"
 export PYTHONPATH=$WORKDIR
 
 TASK=${1}
@@ -93,7 +93,7 @@ CUDA_VISIBLE_DEVICES=${GPU} \
   --do_train --do_eval --do_eval_bleu --do_test ${MULTI_TASK_AUG}  \
   --task ${TASK} --sub_task ${SUB_TASK} --model_type ${MODEL_TYPE} --data_num ${DATA_NUM}  \
   --num_train_epochs ${EPOCH} --warmup_steps ${WARMUP} --learning_rate ${LR}e-5 --patience ${PATIENCE} \
-  --tokenizer_name=${TOKENIZER}  --model_name_or_path=${MODEL_PATH} --data_dir ${DATADIR}/data  \
+  --tokenizer_name=${TOKENIZER}  --model_name_or_path=${MODEL_PATH} --data_dir ${DATADIR}  \
   --cache_path ${CACHE_DIR}  --output_dir ${OUTPUT_DIR}  --summary_dir ${SUMMARY_DIR} \
   --save_last_checkpoints --always_save_model --res_dir ${RES_DIR} --res_fn ${RES_FN} \
   --train_batch_size ${BS} --eval_batch_size ${BS} --max_source_length ${SRC_LEN} --max_target_length ${TRG_LEN} \
