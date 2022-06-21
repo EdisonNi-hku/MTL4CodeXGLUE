7229be506e8e3eaedfc178d0de60ded65a7e0e7e
Ni Jingwei <jingweini8@gmail.com>
2022-06-21 18:58:00 +0100

    code2dataflow

--- code/code_to_ast.py
+++ code/code_to_ast.py
@@ -3,10 +3,73 @@ import os
 import json
 import random
 import math
+import torch
+import multiprocessing
+from transformers import RobertaTokenizer, T5Tokenizer
 from collections import OrderedDict
 from utils import get_filenames
-from evaluator.CodeBLEU.parser import remove_comments_and_docstrings
+from tqdm import tqdm
+from evaluator.CodeBLEU.parser import DFG_python, DFG_java, DFG_ruby, DFG_go, DFG_php, DFG_javascript, DFG_csharp
+from evaluator.CodeBLEU.parser import (remove_comments_and_docstrings,
+                                       tree_to_token_index,
+                                       index_to_code_token,
+                                       tree_to_variable_index)
+
+dfg_function = {
+    'python': DFG_python,
+    'java': DFG_java,
+    'ruby': DFG_ruby,
+    'go': DFG_go,
+    'php': DFG_php,
+    'javascript': DFG_javascript,
+    'c_sharp': DFG_csharp,
+}
+
 root_dir = os.path.dirname(__file__)
+
+
+def get_data_flow(code, parser):
+    try:
+        tree = parser[0].parse(bytes(code, 'utf8'))
+        root_node = tree.root_node
+        tokens_index = tree_to_token_index(root_node)
+        code = code.split('\n')
+        code_tokens = [index_to_code_token(x, code) for x in tokens_index]
+        index_to_code = {}
+        for idx, (index, code) in enumerate(zip(tokens_index, code_tokens)):
+            index_to_code[index] = (idx, code)
+        try:
+            DFG, _ = parser[1](root_node, index_to_code, {})
+        except:
+            DFG = []
+        DFG = sorted(DFG, key=lambda x: x[1])
+        indexs = set()
+        for d in DFG:
+            if len(d[-1]) != 0:
+                indexs.add(d[1])
+            for x in d[-1]:
+                indexs.add(x)
+        new_DFG = []
+        for d in DFG:
+            if d[1] in indexs:
+                new_DFG.append(d)
+        codes = code_tokens
+        dfg = new_DFG
+    except:
+        codes = code.split()
+        dfg = []
+    # merge nodes
+    dic = {}
+    for d in dfg:
+        if d[1] not in dic:
+            dic[d[1]] = d
+        else:
+            dic[d[1]] = (d[0], d[1], d[2], list(set(dic[d[1]][3] + d[3])), list(set(dic[d[1]][4] + d[4])))
+    DFG = []
+    for d in dic:
+        DFG.append(dic[d])
+    dfg = DFG
+    return dfg, code_tokens
 
 
 def load_code(data_root, task, subtask):
@@ -26,9 +89,7 @@ def load_summarization_code(data_root, subtask):
     filename = get_filenames(data_root, 'summarize', subtask, 'train')
     code_strings = []
     with open(filename, encoding='utf-8') as f:
-        for idx, line in enumerate(f):
-            if idx >= 1:
-                break
+        for idx, line in tqdm(enumerate(f), desc='summarize_'+subtask):
             line = line.strip()
             js = json.loads(line)
             original_code = js['code']
@@ -48,19 +109,15 @@ def load_translation_code(data_root, subtask):
     src_filename = filename.split(',')[0]
     trg_filename = filename.split(',')[1]
     src_code_strings = []
-    tgt_code_strings = []
     with open(src_filename) as f1, open(trg_filename) as f2:
-        for line1, line2 in zip(f1, f2):
+        for line1, line2 in tqdm(zip(f1, f2), desc='translate_'+subtask):
             src = line1.strip()
-            trg = line2.strip()
             try:
                 src = remove_comments_and_docstrings(src, src_lang)
-                trg = remove_comments_and_docstrings(trg, tgt_lang)
             except:
                 pass
             src_code_strings.append(src)
-            tgt_code_strings.append(trg)
-    return src_code_strings, tgt_code_strings
+    return src_code_strings
 
 
 def load_refine_code(data_root, subtask):
@@ -69,26 +126,22 @@ def load_refine_code(data_root, subtask):
     src_filename = filename.split(',')[0]
     trg_filename = filename.split(',')[1]
     src_code_strings = []
-    tgt_code_strings = []
     with open(src_filename) as f1, open(trg_filename) as f2:
-        for line1, line2 in zip(f1, f2):
+        for line1, line2 in tqdm(zip(f1, f2), desc='refine_'+subtask):
             src = line1.strip()
-            trg = line2.strip()
             try:
                 src = remove_comments_and_docstrings(src, 'java')
-                trg = remove_comments_and_docstrings(trg, 'java')
             except:
                 pass
-        src_code_strings.append(src)
-        tgt_code_strings.append(trg)
-    return src_code_strings, tgt_code_strings
+            src_code_strings.append(src)
+    return src_code_strings
 
 
 def load_defect_code(data_root, subtask):
     filename = get_filenames(data_root, 'defect', subtask, 'train')
     code_strings = []
     with open(filename, encoding="utf-8") as f:
-        for idx, line in enumerate(f):
+        for idx, line in tqdm(enumerate(f), desc='defect'):
             line = line.strip()
             js = json.loads(line)
             code = js['func']
@@ -104,7 +157,7 @@ def load_clone_code(data_root, subtask):
     filename = get_filenames(data_root, 'clone', subtask, 'train')
     code_strings = []
     with open('/'.join(filename.split('/')[:-1]) + '/data.jsonl') as f:
-        for line in f:
+        for line in tqdm(f, desc='clone'):
             line = line.strip()
             js = json.loads(line)
             code = js['func']
@@ -114,25 +167,6 @@ def load_clone_code(data_root, subtask):
                 pass
             code_strings.append(code)
     return code_strings
-
-
-def code_to_ast_string(code_string, parser):
-    tree = parser.parse(bytes(code_string, "utf8"))
-    root_node = tree.root_node
-
-    def ast2string(root):
-        seq = ""
-        name = root.type
-        if len(root.children) == 0:
-            seq += name + ' '
-        else:
-            seq += name + "|left "
-            for child in root.children:
-                seq += ast2string(child) + ' '
-            seq += name + "|right "
-        return seq
-
-    return ast2string(root_node)
 
 
 def find_identifiers(code, code_lines, node):
@@ -185,27 +219,212 @@ def mask_identifiers(code_string, parser):
 
 def get_parser(lang_dir, lang):
     lang = Language(lang_dir, lang)
-    parser = Parser().set_language(lang)
+    parser = Parser()
+    parser.set_language(lang)
     return parser
 
 
+LANGUAGES = ['python', 'java', 'php', 'c_sharp', 'javascript', 'php', 'ruby', 'go']
+LANG_DIR = root_dir + '/evaluator/CodeBLEU/parser/my-languages.so'
+PARSERS = {}
+for lang in LANGUAGES:
+    PARSERS[lang] = (get_parser(LANG_DIR, lang), dfg_function[lang])
+
+
+def code_to_ast_string(item):
+    code_string, lang = item
+    tree = PARSERS[lang][0].parse(bytes(code_string, "utf8"))
+    root_node = tree.root_node
+
+    def ast2string(root):
+        seq = ""
+        name = root.type
+        if len(root.children) == 0:
+
+            seq += name
+        else:
+            seq += name + "{"
+            for child in root.children:
+                seq += ast2string(child) + '|'
+            seq += name + "}"
+        return seq
+
+    return ast2string(root_node)
+
+
+def is_constant(element) -> bool:
+    if element.startswith("\"") and element.endswith("\""):
+        return True
+    elif element.startswith("\'") and element.endswith("\'"):
+        return True
+    else:
+        try:
+            float(element)
+            return True
+        except ValueError:
+            return False
+
+
+def code_to_dataflow_string(item):
+    code_string, lang = item
+    parser = PARSERS[lang]
+    dfg, code_tokens = get_data_flow(code_string, parser)
+    var_order = {}
+    id2var = {}
+    for t in dfg:
+        id2var[t[1]] = t[0]
+        if is_constant(t[0]):
+            continue
+        else:
+            if t[0] in var_order.keys():
+                if t[1] not in var_order[t[0]].keys():
+                    count = len(var_order[t[0]].keys())
+                    var_order[t[0]][t[1]] = count
+            else:
+                var_order[t[0]] = {}
+                var_order[t[0]][t[1]] = 0
+    dataflow = ""
+    for t in dfg:
+        if is_constant(t[0]):
+            continue
+        else:
+            dataflow += t[0] + str(var_order[t[0]][t[1]]) + ' '
+            if t[2] == 'comesFrom':
+                dataflow += 'come '
+            else:
+                dataflow += 'compute '
+            for i in range(len(t[4])):
+                if t[4][i] not in id2var.keys():
+                    dataflow += code_tokens[t[4][i]] + ' '
+                elif is_constant(id2var[t[4][i]]):
+                    dataflow += id2var[t[4][i]] + ' '
+                else:
+                    dataflow += id2var[t[4][i]] + str(var_order[id2var[t[4][i]]][t[4][i]]) + ' '
+            dataflow = dataflow[:-1] + ','
+    return dataflow
+
+
+def count_tokens(item):
+    s, tokenizer = item
+    s = s.replace('</s>', '<unk>')
+    s_ids = tokenizer.encode(s)
+    return len(s_ids)
+
+
+def code2ast(code_dict):
+    cpu_count = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(cpu_count)
+    count = {}
+    for k, code_list in code_dict.items():
+        task = k.split('_')[0]
+        subtask = k.split('_')[1]
+        if task == 'summarize':
+            lang = subtask
+        elif task == 'translate':
+            lang = subtask.split('-')[0]
+            if lang == 'cs':
+                lang = 'c_sharp'
+        elif task == 'defect':
+            lang = 'c'
+        else:
+            lang = 'java'
+        items = [(code, lang) for code in code_list]
+        ast_list = pool.map(code_to_ast_string, tqdm(items, total=len(items), desc='parsing ' + task + '_' + subtask))
+        with open('ast/' + '{}_{}'.format(task, subtask) + '.code', 'w') as f1, open('ast/' + '{}_{}'.format(task, subtask) + '.ast', 'w') as f2:
+            f1.writelines(code_list)
+            f2.writelines(ast_list)
+        t5_ast_items = [(ast, t5_tokenizer) for ast in ast_list]
+        codet5_ast_items = [(ast, codet5_tokenizer) for ast in ast_list]
+        t5_code_items = [(code, t5_tokenizer) for code in code_list]
+        codet5_code_items = [(code, codet5_tokenizer) for code in code_list]
+        t5_code_count = pool.map(count_tokens, tqdm(t5_code_items, total=len(t5_code_items), desc='t5 code'))
+        codet5_code_count = pool.map(count_tokens,
+                                     tqdm(codet5_code_items, total=len(codet5_code_items), desc='codet5 code'))
+        t5_ast_count = pool.map(count_tokens, tqdm(t5_ast_items, total=len(t5_ast_items), desc='t5 ast'))
+        codet5_ast_count = pool.map(count_tokens,
+                                     tqdm(codet5_ast_items, total=len(codet5_ast_items), desc='codet5 ast'))
+        count[k] = (sum(t5_code_count)/len(t5_code_count),
+                    sum(codet5_code_count) / len(codet5_code_count),
+                    sum(t5_ast_count) / len(t5_ast_count),
+                    sum(codet5_ast_count) / len(codet5_ast_count))
+    print(count)
+    torch.save(count, 'token_count')
+    pool.close()
+
+
+def code2df(code_dict):
+    cpu_count = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(cpu_count)
+    count = {}
+    for k, code_list in code_dict.items():
+        task = k.split('_')[0]
+        subtask = k.split('_')[1]
+        if task == 'summarize':
+            lang = subtask
+        elif task == 'translate':
+            lang = subtask.split('-')[0]
+            if lang == 'cs':
+                lang = 'c_sharp'
+        elif task == 'defect':
+            lang = 'c'
+        else:
+            lang = 'java'
+        items = [(code, lang) for code in code_list]
+        df_list = pool.map(code_to_dataflow_string, tqdm(items, total=len(items), desc='parsing ' + task + '_' + subtask))
+        with open('df/' + '{}_{}'.format(task, subtask) + '.code', 'w') as f1, open(
+                'df/' + '{}_{}'.format(task, subtask) + '.df', 'w') as f2:
+            f1.writelines(code_list)
+            f2.writelines(df_list)
+        t5_df_items = [(df, t5_tokenizer) for df in df_list]
+        codet5_df_items = [(df, codet5_tokenizer) for df in df_list]
+        t5_code_items = [(code, t5_tokenizer) for code in code_list]
+        codet5_code_items = [(code, codet5_tokenizer) for code in code_list]
+        t5_code_count = pool.map(count_tokens, tqdm(t5_code_items, total=len(t5_code_items), desc='t5 code'))
+        codet5_code_count = pool.map(count_tokens,
+                                     tqdm(codet5_code_items, total=len(codet5_code_items), desc='codet5 code'))
+        t5_df_count = pool.map(count_tokens, tqdm(t5_df_items, total=len(t5_df_items), desc='t5 ast'))
+        codet5_df_count = pool.map(count_tokens,
+                                    tqdm(codet5_df_items, total=len(codet5_df_items), desc='codet5 ast'))
+        count[k] = (sum(t5_code_count) / len(t5_code_count),
+                    sum(codet5_code_count) / len(codet5_code_count),
+                    sum(t5_df_count) / len(t5_df_count),
+                    sum(codet5_df_count) / len(codet5_df_count))
+    print(count)
+    torch.save(count, 'token_count_df')
+    pool.close()
+
+
 if __name__ == '__main__':
-    languages = ['python', 'java', 'php', 'c', 'c_sharp', 'javascript', 'php', 'ruby']
-    lang_dir = root_dir + '/evaluator/CodeBLEU/parser/my-languages.so'
-    parsers = {}
-    for lang in languages:
-        parsers[lang] = get_parser(lang_dir, lang)
+    codet5_tokenizer = RobertaTokenizer.from_pretrained('Salesforce/codet5-base', cache_dir='cache')
+    t5_tokenizer = T5Tokenizer.from_pretrained('t5-base', cache_dir='cache')
+    random.seed(1234)
+    cache_fn = root_dir + '/code_cache'
+    if os.path.exists(cache_fn):
+        code_dict = torch.load(cache_fn)
+    else:
+        code_dict = {}
+        tasks = ['summarize', 'refine', 'translate', 'clone']
+        for cur_task in tasks:
+            if cur_task == 'summarize':
+                subtasks = ['python', 'java', 'javascript', 'php', 'go', 'ruby']
+            elif cur_task == 'translate':
+                subtasks = ['cs-java', 'java-cs']
+            elif cur_task == 'refine':
+                subtasks = ['small', 'medium']
+            else:
+                subtasks = ['none']
+            for sub in subtasks:
+                code = load_code(data_root='data', task=cur_task, subtask=sub)
+                code = random.sample(code, math.ceil(0.1 * len(code)))
+                code_dict['{}_{}'.format(cur_task, sub)] = code
+
+        torch.save(code_dict, cache_fn)
+
+    code2df(code_dict)
 
 
-    # python_code = load_summarization_code('data', 'python')
-    # java_code = load_summarization_code('data', 'java')
-    # print(python_code[0])
-    # print(code_to_ast_string(python_code[0], python_parser))
-    # #print(mask_identifiers(java_code[0], java_parser))
-    # masked_string, tgt = mask_identifiers(java_code[0], java_parser)
-    # print(java_code[0])
-    # print(masked_string)
-    # print(tgt)
+
+
 
 
 
