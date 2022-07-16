90c63f9db80e6c630f1bbe6ceaad777cdedcf7f7
Ni Jingwei <jingweini8@gmail.com>
2022-07-16 18:18:45 +0100

    MTL data portion

--- code/sample_data.py
+++ code/sample_data.py
@@ -19,32 +19,41 @@ if __name__ == '__main__':
         if task == 'summarize':
             sub_tasks = ['ruby', 'javascript', 'go', 'python', 'java', 'php']
         elif task == 'translate':
-            sub_tasks = ['java-cs', 'cs-java']
+            sub_tasks = ['java-cs']
         elif task == 'refine':
             sub_tasks = ['small', 'medium']
         else:
             sub_tasks = ['none']
         for sub_task in sub_tasks:
-            if sub_task != 'none':
+            if sub_task != 'none' or task != 'translate':
                 sub_task_data_dir = os.path.join(task_data_dir, sub_task)
                 os.mkdir(sub_task_data_dir)
             else:
                 sub_task_data_dir = task_data_dir
 
             train_fn, dev_fn, test_fn = get_filenames('data', task, sub_task)
-            shutil.copy(dev_fn, sub_task_data_dir)
-            shutil.copy(test_fn, sub_task_data_dir)
-            new_train_fn = train_fn.replace('data/', save_dir + '/')
-            with open(train_fn, 'r') as f:
-                lines = f.readline()
-                lines = [line.strip() for line in lines]
+            if task != 'translate':
+                to_copy = [dev_fn, test_fn]
+                to_sample = [train_fn]
+            else:
+                to_copy = dev_fn.split(',').extent(test_fn.split(','))
+                to_sample = train_fn.split(',')
 
-            random.seed(1234)
-            lines = random.sample(lines, math.ceil((args.percentage / 100) * len(lines)))
+            for fn in to_copy:
+                shutil.copy(fn, sub_task_data_dir)
 
-            with open(new_train_fn, 'w') as f:
-                for line in lines:
-                    f.write(line)
-                    f.write('\n')
+            for fn in to_sample:
+                new_fn = fn.replace('data/', save_dir + '/')
+                with open(fn, 'r') as f:
+                    lines = f.readline()
+                    lines = [line.strip() for line in lines]
+
+                random.seed(1234)
+                lines = random.sample(lines, math.ceil((args.percentage / 100) * len(lines)))
+
+                with open(new_fn, 'w') as f:
+                    for line in lines:
+                        f.write(line)
+                        f.write('\n')
 
 
